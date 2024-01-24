300b272327858ed9279730a2168c4482e7da0776
zakisudev@gmail.com <zakisu250@gmail.com>
2024-01-24 15:59:00 +0300

    add Mongodb configuration

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
 
+// Connect Database
+connectDB();
 const app = express();
 
+// Init Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Define Routes
 app.get('/', (req, res) => res.send('Hello World!'));
 
+// Listen to port
 app.listen(port, () => console.log(`App listening on port ${port}!`));
