27e6f04b87a925f53ec35a922c600141d748b786
Zekaria Mohammed <zakisu250@gmail.com>
2024-01-24 11:59:55 +0300

    Initial React Router Dom setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,32 @@
+import {
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Home />}>
+      <Route path="about" element={<About />} />
+      <Route path="signin" element={<SignIn />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="profile" element={<Profile />} />
+    </Route>
+  )
+);
+
 const App = () => {
-  return <h1 className="text-xl">App</h1>;
+  return (
+    <RouterProvider router={router}>
+      <Home />
+    </RouterProvider>
+  );
 };
 
 export default App;
